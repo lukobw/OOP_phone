597b1d455a5eade673ffd0fdfa7cea8eeabc3cee
Lucas <viper20@poczta.fm>
2017-08-16 09:31:53 +0200

    added default values

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-function Phone(brand, price, color) {
+function Phone(brand, price, color, battery, origin) {
     this.brand = brand;
     this.price = price;
     this.color = color;
-    this.battery = '2500mAh';
-    this.origin = 'China';
+    this.battery = battery || '2500mAh';
+    this.origin = origin || 'China';
 }
 
 Phone.prototype.printInfo = function () {
     console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ', price is ' + this.price + '. ' + 'Standard battery ' + this.battery + ' Country of origin: ' + this.origin);
 };
 
-var iPhone6 = new Phone('Apple', '500USD', 'silver');
+var iPhone6 = new Phone('Apple', '500USD', 'silver', '3000mAh');
 var galaxyS6 = new Phone('Samsung', '300EUR', 'black');
-var onePlusOne = new Phone('One', '200PLN', 'gold');
+var onePlusOne = new Phone('One', '200PLN', 'gold', '', 'Singapur');
 
 iPhone6.printInfo();
 galaxyS6.printInfo();
